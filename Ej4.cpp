62f2ccbb601579e06f5e6d98b197de53eec12e75
magdalenagomezf <2302577@ucc.edu.ar>
2024-08-18 10:36:10 -0300

    ej 4 y5

--- Ej4.cpp
+++ Ej4.cpp
@@ -1,3 +1,30 @@
-//
-// Created by Magdalena on 12/8/2024.
-//
+#include <iostream>
+#include <vector>
+using namespace std;
+int mayor( const vector<int>& numero, int n){
+  int may=mayor(numero,n-1);
+
+if(n==1){
+    return numero[0];
+}
+if (may > numero[n-1]){
+    return may;
+}
+else {
+    return numero[n-1];
+}
+
+}
+int main (){
+
+     vector<int>numero;
+     int n;
+    cout << "Ingrese el tamanio"<< endl;
+    cin >> n;
+    cout << "Ingrese los elementos del vector:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> numero[i];
+    }
+    cout << mayor(numero,n);
+
+}
--- Ej5.cpp
+++ Ej5.cpp
@@ -1,3 +1,29 @@
-//
-// Created by Magdalena on 12/8/2024.
-//
+#include <iostream>
+using namespace std;
+bool verificarD(int n, int digito){
+    int numero;
+    if (n==0){
+        return false;
+    }
+    numero=n%10;
+    if (numero==digito)
+    {
+        return true;
+    }else{
+        return verificarD(n / 10, digito);
+    }
+}
+int main(){
+    int n, digito;
+    cout << "INgrese un numero entero"<< endl;
+    cin >> n;
+    cout << "INgrese un solo digito" << endl;
+    cin >>digito;
+    bool resultado= verificarD(n,digito);
+    if (resultado) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+}
